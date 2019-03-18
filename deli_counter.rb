27d8a1acb9ced26e785a1413a2430b4644248dc7
carefree-closure-1550 <leo.kalshteyn@gmail.com>
2019-03-18 18:45:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,15 @@
-def line(array)
-  if array==[]
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli==[]
     puts "The line is currently empty."
   else
     order="The line is currently:"
-    array.each_with_index do |name, index|
+    katz_deli.each_with_index do |name, index|
       order << " #{index + 1}. #{name}"
     end
     puts order
   end
 end
+
+def take_a_number(katz_deli, name)
