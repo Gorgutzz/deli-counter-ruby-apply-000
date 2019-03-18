459cd20c5c2b8bc496facd93822abc4fc2ffff78
carefree-closure-1550 <leo.kalshteyn@gmail.com>
2019-03-18 18:41:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,9 @@ def line(array)
   if array==[]
     puts "The line is currently empty."
   else
-    order=""
+    order="The line is currently: "
     array.each_with_index do |name, index|
-      order << "#{index + 1}. #{name}"
+      order << "#{index + 1}. #{name} "
     end
     puts order
   end
